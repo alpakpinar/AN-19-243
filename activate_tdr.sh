ad0f5fec17e59dab769210713a5170f6f0975dc8
Alp Akpinar <aakpinar@bu.edu>
2019-11-06 22:20:52 -0500

    First commit

---
+++ activate_tdr.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+## Source this file to activate tdr command
+## After that, run the command
+## tdr --temp_dir=./build b 
+## to compile TeX code
+## output will be in build dir
+
+eval `utils/tdr runtime -sh`
+
